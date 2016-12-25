cb2dd83179d1cdfa060c6e800af83bf36539a624
Suntisuk Khemphed <suntisuk.dev@gmail.com>
2016-12-26 00:08:07 +0700

    My first test with chai mocha

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect;
+
+
+describe('A basic chai mmocha test',function(){
+
+	it('should pass where everything is okay',function(){
+		expect(true).to.be.true;
+	});
+
+});
+
